d1981584b1fde1863a6724e660a6145fc27d9168
OmarCloud <9620570+OmarCloud20@users.noreply.github.com>
2021-11-15 23:38:15 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,21 +11,21 @@ tmpfile=$(mktemp)
 
 # Run the commands to update the system and write the log
 # file at the same time.
-echo "sudo aptitupde update" >> ${tmpfile}
-sudo aptitude update >> ${tmpfile} 2>&1
+echo "aptitupde update" >> ${tmpfile}
+aptitude update >> ${tmpfile} 2>&1
 echo "" >> ${tmpfile}
-echo "sudo aptitude safe-upgrade" >> ${tmpfile}
-sudo aptitude -y safe-upgrade >> ${tmpfile} 2>&1
+echo "aptitude safe-upgrade" >> ${tmpfile}
+aptitude -y safe-upgrade >> ${tmpfile} 2>&1
 echo "" >> ${tmpfile}
-echo "sudo aptitude clean" >> ${tmpfile}
-sudo aptitude clean >> ${tmpfile} 2>&1
+echo "aptitude clean" >> ${tmpfile}
+aptitude clean >> ${tmpfile} 2>&1
 
 # Send the temporary log via mail. The fact if the upgrade
 # was succesful or not is written in the subject field.
 if grep -q 'E: \|W: ' ${tmpfile} ; then
-        mail -s "P4 upgrade has failed on $(date)" ${admin_mail} < ${tmpfile}
+        mail -s "RPI-4 upgrade has failed on $(date)" ${admin_mail} < ${tmpfile}
 else
-        mail -s "P4 upgrade is succesfully completed on $(date)" ${admin_mail} < ${tmpfile}
+        mail -s "RPI-4 upgrade is succesfully completed on $(date)" ${admin_mail} < ${tmpfile}
 fi
 
 # Remove the temporary log file in temporary path.
